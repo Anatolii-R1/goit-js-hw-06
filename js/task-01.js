d729a1216f6472dafe47ae3a92e29b08d1cc85ee
Anatolii-R1 <129444880+Anatolii-R1@users.noreply.github.com>
2023-08-03 21:21:17 +0300

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,9 @@
-// const numbCategories = document.querySelectorAll(`.item`);
-// console.log(`Number of categories: ${numbCategories.length}`);
+const numbCategories = document.querySelectorAll(`.item`);
+console.log(`Number of categories: ${numbCategories.length}`);
 
-// const itemsElement = document.querySelectorAll(".item");
-// itemsElement.forEach((item) => {
-//   console.log("Category: ", item.firstElementChild.textContent);
-//   console.log("Elements: ", item.lastElementChild.children.length);
-// });
+const itemsElement = document.querySelectorAll(".item");
+itemsElement.forEach((item) => {
+  console.log("Category: ", item.firstElementChild.textContent);
+  console.log("Elements: ", item.lastElementChild.children.length);
+});
 
