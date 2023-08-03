c8145d4be752a3973d3b7a1ecfad79bdc7e9a616
Anatolii-R1 <129444880+Anatolii-R1@users.noreply.github.com>
2023-08-03 23:18:28 +0300

    6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const inputText = document.querySelector('#validation-input');
+inputText.addEventListener('blur', onBlurBorderColor);
+
+function onBlurBorderColor(event) {
+  const inputDataLength = Number(inputText.dataset.length);
+  const inputValueLength = Number(inputText.value.trim().length);
+
+  if (inputValueLength === inputDataLength) {
+    inputText.classList.add('valid');
+    inputText.classList.remove('invalid');
+  } else {
+    inputText.classList.remove('valid');
+    inputText.classList.add('invalid');
+  }
+}
